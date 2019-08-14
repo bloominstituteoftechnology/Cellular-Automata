2982203cbc93de14d0c02b1abf868be7012bb37a
bcabanayan <bcabanayan@gmail.com>
2019-08-13 18:23:11 -0700

    Added logic to color squares based on current state

--- src/conways.py
+++ src/conways.py
@@ -8,10 +8,10 @@ WIN_SIZE = 500
 
 # 1. set up initial states
 
-# cur_states = [0] * [400]
-# cur_states[10] = 1
-# cur_states[30] = 1
-# cur_states[50] = 1
+cur_states = [0] * 400
+cur_states[10] = 1
+cur_states[30] = 1
+cur_states[50] = 1
 # next_states = []
 
 pygame.init()
@@ -54,19 +54,19 @@ while not done:
  
     # --- Drawing code should go here
     # pygame.draw.rect(surface, color, pygame.Rect(left, top, width, height))
+    cur_index = 0
     x = 5
     while x < 500:
         y = 5
         while y < 500: 
             # 2. draw rectangles based on states 
-            # state = cur_states[cur_index]
-            # if state == 0:
-                # pygame.draw.rect(screen, BLACK, pygame.Rect(x, y, 20, 20))
-            # else:
-                # pygame.draw.rect(screen, WHITE, pygame.Rect(x, y, 20, 20))
+            state = cur_states[cur_index]
+            if state == 0:
+                pygame.draw.rect(screen, BLACK, pygame.Rect(x, y, 20, 20))
+            else:
+                pygame.draw.rect(screen, WHITE, pygame.Rect(x, y, 20, 20))
             # 4. draw based on values in next_state
-            pygame.draw.rect(screen, WHITE, pygame.Rect(x, y, 20, 20))
-            # cur_index += 1
+            cur_index += 1
             y += 25
         x += 25
 
