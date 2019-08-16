d4f90406bc97f6eea59606f1895ef0774710efc9
bcabanayan <bcabanayan@gmail.com>
2019-08-15 18:38:41 -0700

    Clean up of logic to calculate new state based on current state and render to the game

--- src/conways.py
+++ src/conways.py
@@ -7,7 +7,7 @@ BLUE = (0 , 0, 255)
 GRAY = (25, 25, 25)
 WIN_SIZE = 500
 
-# 1. set up initial states
+# 1. v.1 set up initial states
 
 cur_states = [0] * 400
 cur_states[10] = 1
@@ -50,10 +50,12 @@ while not done:
     # --- Game logic should go here
     
     generation += 1
-    
+
     # PAUSE/PLAY
 
-    if event.type == pygame.MOUSEBUTTONDOWN and # our button clicked ( look at x, y of click, compare to area of buttons; if area is clicked, then halt the logic)
+    if event.type == pygame.MOUSEBUTTONDOWN:
+        click_pos = pygame.mouse.get_pos()
+        if pause_button.collidepoint(click_pos):
             is_paused = not is_paused
 
     # have a boolean keeping track of if game is paused or stopped...
@@ -62,16 +64,9 @@ while not done:
 
     # RESTART
 
-    if event.type == pygame.MOUSEBUTTONDOWN and # our button clicked ( look at x, y of click, compare to area of buttons; if area is clicked, then set generation -> 0, set current state to initial states)
-    is_paused = not is_paused
+    # if event.type == pygame.MOUSEBUTTONDOWN and our button clicked ( look at x, y of click, compare to area of buttons; if area is clicked, then set generation -> 0, set current state to initial states)
 
-    # have a boolean keeping track of if game is paused or stopped...
-    # could toggle pause button into a play button
-    # could have text be dynamic depending on the state, or put play/pause
-
-
-    # 3. night 2: work on rules that i) look at all neighbors, ii) save new state in 
-    # next_states[]
+    # is_paused = not is_paused
 
     # MAIN SIMULATION LOGIC!!!!
 
@@ -102,70 +97,63 @@ while not done:
 
     # MY LOGIC TO CHECK NEIGHBORS
 
-
-
-    current_index = 29
-    # ADD LOGIC TO CHECK NEIGHBORS DEPENDING ON CURRENT INDEX! corners, upper 
-    # a. find cur_rect's neighbors
-    # b. find neighbor states
-    neighbors = []
-    # CORNER NEIGHBORS
-    if current_index == 0:
-        neighbors = [1, 20, 21]
-    elif current_index == 19:
-        neighbors = [18, 38, 39]
-    elif current_index == 380:
-        neighbors = [360, 361, 381]
-    elif current_index == 399:
-        neighbors = [378, 379, 398]
-    # BORDER NEIGHBORS
-    elif current_index in range(1, 19):
-        neighbor_values = [-1, +1, 19, 20, 21]
-        neighbors = [current_index + i for i in neighbor_values]
-    elif current_index in range(381, 399):
-        neighbor_values = [-21, -20, -19, -1, +1]
-        neighbors = [current_index + i for i in neighbor_values]
-    elif current_index % 20 == 0:
-        neighbor_values = [-20, -19, -1, 20, 21]
-        neighbors = [current_index + i for i in neighbor_values]
-        print('works???')
-    elif (current_index + 1) % 20 == 0:
-        neighbor_values = [-21, -20, -1, 19, 20,]
-        neighbors = [current_index + i for i in neighbor_values]
-    # ALL OTHER NEIGHBORS
-    else:
-        neighbor_values = [-21, -20, -19, -1, +1, 19, 20, 21]
-        neighbors = [current_index + i for i in neighbor_values]
-    print(neighbors)
-    # c. sum neighbor states
-    neighbor_states = [cur_states[i] for i in neighbors]
-    print(neighbor_states)
-    state_sum = 0 
-    for i in neighbor_states:
-        state_sum += i
-    print(state_sum)
-    # *** could move this logic to a separate helper function
-
-    # d. update cur_rect state based on sum of neighbor states
-
     new_states = [None] * 400
 
-    if cur_states[current_index] == 1:
-        # Any live cell with fewer than two live neighbours dies, as if by underpopulation.
-        # Any live cell with more than three live neighbours dies, as if by overpopulation.
-        if state_sum < 2 or state_sum > 3:
-            new_states[current_index] = 0
-        # Any live cell with two or three live neighbours lives on to the next generation.
+    for current_index in range(len(cur_states)):
+        neighbors = []
+        # CORNER NEIGHBORS
+        if current_index == 0:
+            neighbors = [1, 20, 21]
+        elif current_index == 19:
+            neighbors = [18, 38, 39]
+        elif current_index == 380:
+            neighbors = [360, 361, 381]
+        elif current_index == 399:
+            neighbors = [378, 379, 398]
+        # BORDER NEIGHBORS
+        elif current_index in range(1, 19):
+            neighbor_values = [-1, +1, 19, 20, 21]
+            neighbors = [current_index + i for i in neighbor_values]
+        elif current_index in range(381, 399):
+            neighbor_values = [-21, -20, -19, -1, +1]
+            neighbors = [current_index + i for i in neighbor_values]
+        elif current_index % 20 == 0:
+            neighbor_values = [-20, -19, -1, 20, 21]
+            neighbors = [current_index + i for i in neighbor_values]
+        elif (current_index + 1) % 20 == 0:
+            neighbor_values = [-21, -20, -1, 19, 20,]
+            neighbors = [current_index + i for i in neighbor_values]
+        # ALL OTHER NEIGHBORS
         else:
-            new_states[current_index] = 1
-    else:
-        # Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
-        if state_sum == 3:
-            new_states[current_index] = 1
-        else:
-            new_states[current_index] = 0
+            neighbor_values = [-21, -20, -19, -1, +1, 19, 20, 21]
+            neighbors = [current_index + i for i in neighbor_values]
+        # c. sum neighbor states
+        neighbor_states = [cur_states[i] for i in neighbors]
+        
+        state_sum = 0 
 
- 
+        for i in neighbor_states:
+            state_sum += i
+            
+        # *** could move this logic to a separate helper function
+
+        # d. update cur_rect state based on sum of neighbor states
+
+        if cur_states[current_index] == 1:
+            # Any live cell with fewer than two live neighbours dies, as if by underpopulation.
+            # Any live cell with more than three live neighbours dies, as if by overpopulation.
+            if state_sum < 2 or state_sum > 3:
+                new_states[current_index] = 0
+            # Any live cell with two or three live neighbours lives on to the next generation.
+            else:
+                new_states[current_index] = 1
+        else:
+            # Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
+            if state_sum == 3:
+                new_states[current_index] = 1
+            else:
+                new_states[current_index] = 0
+    cur_states = new_states
 
     # --- Screen-clearing code goes here
  
